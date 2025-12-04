382135378b140fd2c9eae172b56d0c93de04ebca
Rei Mici <rei.mici2@studio.unibo.it>
2025-12-04 13:12:53 +0100

    Fix of AppFooter

--- frontend/src/components/AppFooter.vue
+++ frontend/src/components/AppFooter.vue
@@ -4,10 +4,10 @@
 <template>
   <footer class="bg-[#151e2b] text-white py-6 mt-auto w-full z-10">
     <div class="flex justify-center space-x-8 text-sm font-medium tracking-wide">
-      <a href="#" class="hover:text-[#3b76ad] transition">Chi Siamo</a>
-      <a href="#" class="hover:text-[#3b76ad] transition">Contatti</a>
-      <a href="#" class="hover:text-[#3b76ad] transition">Privacy</a>
-      <a href="#" class="hover:text-[#3b76ad] transition">Termini</a>
+      <router-link to="/about" class="text-[#3b76ad] font-bold cursor-default">Chi Siamo</router-link>
+      <router-link to="/contact" class="text-[#3b76ad] font-bold cursor-default">Contatti</router-link>
+      <router-link to="/privacy" class="text-[#3b76ad] font-bold cursor-default">Privacy</router-link>
+      <router-link to="/terms" class="text-[#3b76ad] font-bold cursor-default">Termini</router-link>
     </div>
   </footer>
 </template>
